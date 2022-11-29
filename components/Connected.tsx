197580c09046836f104597abc7394b8773a67f3f
Rafa <rmoreno.cesar@gmail.com>
2022-11-28 20:51:52 -0600

    first commit

---
+++ components/Connected.tsx
@@ -0,0 +1,75 @@
+import { FC, MouseEventHandler, useCallback } from "react"
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
+import { useWalletModal } from "@solana/wallet-adapter-react-ui"
+import { useWallet } from "@solana/wallet-adapter-react"
+
+
+const Connected: FC = () => {
+
+const modalState = useWalletModal()
+  const { wallet, connect } = useWallet()
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (event) => {
+      if (event.defaultPrevented) {
+        return
+      }
+
+      if (!wallet) {
+        modalState.setVisible(true)
+      } else {
+        connect().catch(() => {})
+      }
+    },
+    [wallet, connect, modalState]
+  )
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Welcome Buildoor.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each buildoor is randomly generated and can be staked to receive
+            <Text as="b"> $BLD</Text> Use your <Text as="b"> $BLD</Text> to
+            upgrade your buildoor and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        <Image src="avatar1.png" alt="" />
+        <Image src="avatar2.png" alt="" />
+        <Image src="avatar3.png" alt="" />
+        <Image src="avatar4.png" alt="" />
+        <Image src="avatar5.png" alt="" />
+      </HStack>
+
+      <Button bgColor="accent" color="white" maxW="380px">
+        <HStack>
+          <Text>mint buildoor</Text>
+          <ArrowForwardIcon />
+        </HStack>
+      </Button>
+    </VStack>
+  )
+}
+
+export default Connected
